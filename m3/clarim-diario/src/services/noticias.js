6575b991df9805467793d80c7d14218d464d45a2
Vinícios Mendes <viniciosmendes908@gmail.com>
2026-09-22 17:32:29 -0300

    feat: usa URL da API por variavel de ambiente no servico de noticias

--- m3/clarim-diario/src/services/noticias.js
+++ m3/clarim-diario/src/services/noticias.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
-const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+//const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/',
+    baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080',
 });
 export async function listarNoticias() {
-    await esperar(1000);
-    const { data } = await api.get('/noticias');
+    //await esperar(1000);
+    const { data } = await api.get('api/noticias');
     return data;
-}   
+}
 export async function buscarNoticia(id) {
-    await esperar(1000);
-    const { data } = await api.get(`/noticias/${id}`);
+    // await esperar(1000);
+    const { data } = await api.get(`api/noticias/${id}`);
     return data;
 }
